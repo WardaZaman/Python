ec458788600f0852ed8adeaad12060e40f104658
WardaZaman <warda.zaman@ac.isb.edu.bn>
2016-10-26 08:50:28 +0800

    Add files via upload

---
+++ Random guessing number.py
@@ -0,0 +1,12 @@
+#code GC01
+number = input ('Choose a number between 1 and 100'):
+    if number < 1:
+        print ('The number is low')
+    elif number > 100:
+        print ('The number is high')
+    else:
+        print ('Keep guessing')
+
+    
+    
+    
---
+++ Simple loops.py
@@ -0,0 +1,13 @@
+#Exercise 1
+for i in range (1, 101):
+    print (i)
+
+#Exercise 2
+for i in range (1, 102, 2):
+    print (i)
+    
+#exercise 3
+for i in range (1000, 1, -10):
+    print (i)
+
+    
